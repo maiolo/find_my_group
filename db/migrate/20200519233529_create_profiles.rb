4705c3045d59e5dfc09691a1ecbd9482fd2bd2ef
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-05-19 20:44:33 -0300

    First Commit

---
+++ db/migrate/20200519233529_create_profiles.rb
@@ -0,0 +1,18 @@
+class CreateProfiles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profiles do |t|
+      t.string :nickname, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.text :description
+      t.string :gender, null: false
+      t.string :favorite_role, null: false
+      t.string :country
+      t.string :state
+      t.string :city
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_232919) do
+ActiveRecord::Schema.define(version: 2020_05_19_233529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.text "description"
+    t.string "gender", null: false
+    t.string "favorite_role", null: false
+    t.string "country"
+    t.string "state"
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_05_19_232919) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
