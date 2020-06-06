9e1609a76df11eba7af9fb873244397c6e69733b
Lucas Lima <lucasrlima@gmail.com>
2020-06-06 15:50:34 -0300

    chat with bugs

---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,18 @@
+import consumer from "./consumer";
+
+
+const initChatroomCable = () => {
+  console.log("oi")
+  const messagesContainer = document.getElementById('messages');
+  if (messagesContainer) {
+    const id = messagesContainer.dataset.chatroomId;
+
+    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
+      received(data) {
+        console.log(data); // called when data is broadcast in the cable
+      },
+    });
+  }
+}
+
+export { initChatroomCable };
