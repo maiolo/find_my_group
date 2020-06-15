e283c72476813de01a0a2a0f8b5565902d99895e
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-06-15 19:47:19 -0300

    some adjusments on style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,12 @@ require('@rails/activestorage').start();
 require('channels');
 
 import 'bootstrap';
+// import Swal from 'sweetalert2/dist/sweetalert2.js';
 import { initChatroomCable } from '../channels/chatroom_channel';
 
 
 document.addEventListener('turbolinks:load', () => {
   initChatroomCable();
 });
+
+// window.Swal = Swal;
